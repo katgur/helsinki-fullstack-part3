6bf45b523a6ee87771fd2a69753b75e9267c21c0
katgur <mitsutanaka29@gmail.com>
2023-09-12 21:57:40 +0300

    3.17*: Phonebook database, step5

--- index.js
+++ index.js
@@ -8,30 +8,30 @@ const Person = require('./model/person')
 const app = express()
 
 // middleware
+app.use(express.json())
+app.use(cors())
+app.use(express.static('dist'))
 morgan.token('body', req => {
     if (req.method === 'POST') {
         return JSON.stringify(req.body)
     }
 })
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'))
-app.use(express.json())
-app.use(cors())
-app.use(express.static('dist'))
 
 // routes
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response, next) => {
     Person.find({})
+        // new Promise(() => {
+        //     throw new Error('kek')
+        // })
         .then(result => {
             response.json(result)
             mongoose.connection.close()
         })
-        .catch(error => {
-            console.log('error getting persons: ', error.message)
-            response.status(500).end()
-        })
+        .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id)
         .then(result => {
             if (result) {
@@ -40,13 +40,10 @@ app.get('/api/persons/:id', (request, response) => {
                 response.status(404).end()
             }
         })
-        .catch(error => {
-            console.log('error getting person: ', error.message)
-            response.status(500).end()
-        })
+        .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     if (!body) {
@@ -73,13 +70,10 @@ app.post('/api/persons', (request, response) => {
         .then(result => {
             response.status(201).json(person)
         })
-        .catch(error => {
-            console.log('error saving person: ', error.message)
-            response.status(500).end()
-        })
+        .catch(error => next(error))
 })
 
-app.put('/api/persons/:id', (request, response) => {
+app.put('/api/persons/:id', (request, response, next) => {
     const body = request.body
 
     if (!body) {
@@ -107,25 +101,19 @@ app.put('/api/persons/:id', (request, response) => {
                 response.status(404).end()
             }
         })
-        .catch(error => {
-            console.log('error updating person: ', error.message)
-            response.status(500).end()
-        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     const id = request.params.id
     Person.findByIdAndRemove(id)
         .then(result => {
             response.status(204).end()
         })
-        .catch(error => {
-            console.log('error deleting person: ', error.message)
-            response.status(500).end()
-        })
+        .catch(error => next(error))
 })
 
-app.get('/info', (request, response) => {
+app.get('/info', (request, response, next) => {
     Person.count({})
         .then(result => {
             const info = `Phonebook has info for ${result} people`
@@ -134,11 +122,21 @@ app.get('/info', (request, response) => {
                 `<div>${info}<br/>${timestamp}</div>`
             )
         })
-        .catch(error => {
-            console.log('error getting count of persons: ', error.message)
-            response.status(500).end()
-        })
+        .catch(error => next(error))
 })
+
+function errorHandler(error, request, response, next) {
+    console.error(error)
+
+    if (error.name === 'CastError') {
+        response.status(400).send({ error: 'malformatted id' })
+    } else {
+        response.status(500).end()
+    }
+
+    next()
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
