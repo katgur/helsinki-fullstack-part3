422b68400b059720dffed0af0d464aca10d9f759
katgur <mitsutanaka29@gmail.com>
2023-12-01 19:12:38 +0300

    fixed alert bg

--- client/src/component/Alert.jsx
+++ client/src/component/Alert.jsx
@@ -1,7 +1,6 @@
 function Alert({ message }) {
-    const color = message && message.type === 'error' ? 'red' : 'green'
     return (
-        message && <div className={`absolute bottom-2 right-2 rounded bg-${color}-100 w-[300px] text-black p-5 border-l-${color}-700 border-l-4`}>{message.text}</div>
+        message && <div className={`absolute bottom-2 right-2 rounded w-[300px] text-black p-5 border-l-4 ${message.type}`}>{message.text}</div>
     )
 }
 
